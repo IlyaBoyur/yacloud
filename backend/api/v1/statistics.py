7b137dab700faa941d4c96ef47c5287f8943a265
Ilya Boyur <i.boyur@idaproject.com>
2023-06-16 19:46:26 +0300

    Add dummy /ping API

---
+++ backend/api/v1/statistics.py
@@ -0,0 +1,16 @@
+import logging
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from db.db import get_session
+from schemas.status import Status
+
+router = APIRouter()
+logger = logging.getLogger(__name__)
+
+
+@router.get("/ping", response_model=Status)
+async def ping(*, db: AsyncSession = Depends(get_session)) -> Status:
+    """Get service status"""
+    return {"message": "test"}
