93f44bcfa17cafbf83381971969591765db78d24
Ilya Boyur <i.boyur@idaproject.com>
2023-06-16 18:41:50 +0300

    Upgrade base to sqlalchemy 2.0

--- backend/db/db.py
+++ backend/db/db.py
@@ -1,12 +1,17 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 from core.config import app_settings
 
 
-Base = declarative_base()
+class Base(DeclarativeBase):
+    pass
+
+
 engine = create_async_engine(app_settings.project_db, echo=True, future=True)
-async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+async_session = sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False
+)
 
 
 async def get_session() -> AsyncSession:
