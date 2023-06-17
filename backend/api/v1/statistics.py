7d5c6e75b62a1807e45cdfafea7ed77a272b841c
Ilya Boyur <ilya.boyur.r@ya.ru>
2023-06-17 17:00:48 +0300

    Add database status response to ping

--- backend/api/v1/statistics.py
+++ backend/api/v1/statistics.py
@@ -1,16 +1,30 @@
 import logging
+import time
 
 from fastapi import APIRouter, Depends
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.sql import functions
 
 from db import get_session
-from schemas.status import Status
+from schemas.status import Status, StatusError
 
 router = APIRouter()
 logger = logging.getLogger(__name__)
 
+DB_CONNECTION_ERROR = "Database connection error occurred."
+
 
 @router.get("/ping", response_model=Status)
-async def ping(*, db: AsyncSession = Depends(get_session)) -> Status:
+async def ping(
+    *, db: AsyncSession = Depends(get_session)
+) -> Status | StatusError:
     """Get service status"""
-    return {"message": "test"}
+    try:
+        start = time.perf_counter()
+        await db.execute(statement=select(functions.now()))
+        elapsed = time.perf_counter() - start
+        return Status(db=elapsed)
+    except ConnectionError as error:
+        logger.exception(error)
+        return StatusError(detail=DB_CONNECTION_ERROR)
