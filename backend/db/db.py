0cd6eb0ae544816d6440197467ca984424a843a2
Ilya Boyur <ilya.boyur.r@ya.ru>
2023-06-17 13:13:08 +0300

    Create db schema

--- backend/db/db.py
+++ backend/db/db.py
@@ -1,11 +1,26 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    Mapped,
+    declared_attr,
+    mapped_column,
+    sessionmaker,
+)
 
 from core.config import app_settings
 
 
 class Base(DeclarativeBase):
-    pass
+    """
+    Define common elements that may be applied to mapped
+    classes using this class as a base class.
+    """
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower()
+
+    id: Mapped[int] = mapped_column(primary_key=True)
 
 
 engine = create_async_engine(app_settings.project_db, echo=True, future=True)
--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -9,6 +9,7 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from db.db import Base
+from models import User, UserFile
 
 load_dotenv(".env")
 
