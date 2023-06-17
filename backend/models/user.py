74847181e95481509ada1226d32f8d3d314da500
Ilya Boyur <i.boyur@idaproject.com>
2023-06-17 13:13:08 +0300

    Create db schema

---
+++ backend/models/user.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from fastapi_users.db import SQLAlchemyBaseUserTableUUID
+from sqlalchemy import DateTime
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.sql.expression import func
+
+from db.db import Base
+
+
+class User(SQLAlchemyBaseUserTableUUID, Base):
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime, index=True, default=func.now(), nullable=False
+    )
+
+    files: Mapped[list["UserFile"]] = relationship(
+        back_populates="user", cascade="all, delete"
+    )
+
+    def __repr__(self):
+        return f"User(id={self.id}, {self.email})"
---
+++ backend/models/user_file.py
@@ -0,0 +1,29 @@
+import uuid
+from datetime import datetime
+
+from sqlalchemy import Boolean, DateTime, ForeignKey, Integer, String, Uuid
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.sql.expression import func
+
+from db.db import Base
+
+
+class UserFile(Base):
+    id: Mapped[uuid.UUID] = mapped_column(
+        Uuid, primary_key=True, default=uuid.uuid4
+    )
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    name: Mapped[uuid.UUID] = mapped_column(String(255), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime, index=True, default=func.now(), nullable=False
+    )
+    path: Mapped[str] = mapped_column(String(1024), default=0, nullable=False)
+    size: Mapped[int] = mapped_column(Integer, default=0, nullable=False)
+    is_downloadable: Mapped[bool] = mapped_column(
+        Boolean, default=True, nullable=False
+    )
+
+    user: Mapped["User"] = relationship(back_populates="files")
+
+    def __repr__(self):
+        return f"File(id={self.id}, {self.name})"
