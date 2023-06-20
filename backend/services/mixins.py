c0843047cb92ad22ccb8388b5676ae8a3354d53a
Ilya Boyur <ilya.boyur.r@ya.ru>
2023-06-20 21:06:03 +0300

    Move db access mixin in separate module

---
+++ backend/services/mixins.py
@@ -0,0 +1,19 @@
+import logging
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.sql import functions
+
+logger = logging.getLogger(__name__)
+
+
+class RepositoryDbLatencyMixin:
+    @staticmethod
+    async def get_current_time(db: AsyncSession) -> str:
+        statement = select(functions.now())
+        try:
+            result = await db.execute(statement=statement)
+            return result.scalar()
+        except ConnectionRefusedError as error:
+            logger.error(error)
+            return ""
--- backend/services/statistics.py
+++ backend/services/statistics.py
@@ -1,25 +1,7 @@
-import logging
-
-from sqlalchemy import select
-from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.sql import functions
-
-logger = logging.getLogger(__name__)
+from .mixins import RepositoryDbLatencyMixin
 
 
-class RepositoryStatisticsMixin:
-    @staticmethod
-    async def get_current_time(db: AsyncSession) -> str:
-        statement = select(functions.now())
-        try:
-            result = await db.execute(statement=statement)
-            return result.scalar()
-        except ConnectionRefusedError as error:
-            logger.error(error)
-            return ""
-
-
-class RepositoryStatistics(RepositoryStatisticsMixin):
+class RepositoryStatistics(RepositoryDbLatencyMixin):
     pass
 
 
