4c4d47235f38fdc1f7e57dfa1df9a6f50b3888a3
Ilya Boyur <i.boyur@idaproject.com>
2023-06-25 21:14:53 +0300

    Add cache statistics

--- backend/api/v1/statistics.py
+++ backend/api/v1/statistics.py
@@ -1,12 +1,12 @@
 import logging
-import time
 
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from db import get_session
 from schemas.status import Status, StatusError
-from services import statistics_service
+from services import Cache, get_cache, statistics_service
+from utils import elapse
 
 router = APIRouter()
 logger = logging.getLogger(__name__)
@@ -16,14 +16,17 @@ DB_CONNECTION_ERROR = "Database connection error occurred."
 
 @router.get("/ping", response_model=Status)
 async def ping(
-    *, db: AsyncSession = Depends(get_session)
+    *,
+    db: AsyncSession = Depends(get_session),
+    cache: Cache = Depends(get_cache)
 ) -> Status | StatusError:
     """Get service status"""
     try:
-        start = time.perf_counter()
-        await statistics_service.get_current_time(db)
-        elapsed = time.perf_counter() - start
-        return Status(db=elapsed)
-    except ConnectionError as error:
-        logger.exception(error)
+        status = Status(
+            db=await elapse(statistics_service.get_current_time(db)),
+            cache=await elapse(cache.ping()),
+        )
+        return status
+    except ConnectionError:
+        logger.exception(DB_CONNECTION_ERROR)
         return StatusError(detail=DB_CONNECTION_ERROR)
--- backend/schemas/status.py
+++ backend/schemas/status.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 
 class Status(BaseModel):
     db: float
+    cache: float
 
 
 class StatusError(BaseModel):
